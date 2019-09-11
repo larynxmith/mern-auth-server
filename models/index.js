0c5e58392c3c4968797641f93d4563803329777b
larynxmith <larynxmith@gmail.com>
2019-09-11 14:29:47 -0700

    added mongo to index model

---
+++ models/index.js
@@ -0,0 +1,10 @@
+let mongoose = require('mongoose')
+
+mongoose.connect(proces.env.MONGODB_URI || 'mongodb://localhost/mern-sei-26', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+
+
+module.exports.User = require('./user')
