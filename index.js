2f50eb5a7d5a87b748cbd77a91e3fa4a8a9a3ed6
larynxmith <larynxmith@gmail.com>
2019-09-11 14:51:51 -0700

    rowdy logger added

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 let cors = require('cors')
 let express = require('express')
 let morgan = require('morgan')
+let rowdyLogger = require('rowdy-logger')
 
 //instance
 let app = express()
+let rowdyResults = rowdyLogger.begin(app)
 
 //middleware
 app.use(cors())
@@ -12,11 +14,15 @@ app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json({ limit: '10mb' }))
 
-//Catch-All route
+
+// routes
 app.use('/auth', require('./controllers/auth'))
+//Catch-All route
 app.get('*', (req, res) => {
     res.status(404).send({ message: 'Not Found' })
 })
 
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, () => {
+    rowdyResults.print()
+})
